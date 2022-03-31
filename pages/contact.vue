91a0d650b7e515c3b5deccddc0fd5bf541155093
Dylan K <50585244+dylankainth@users.noreply.github.com>
2022-03-31 15:54:30 +0100

    added to contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -20,97 +20,41 @@
     </div>
 
     <div class="px-4 py-5 my-5 text-center">
-      <h1 class="display-6 fw-bold">
-        A Team of Motorsport, Engineering, Designing and Business Enthusiasts
-      </h1>
+      <h1 class="display-6 fw-bold">Get in touch.</h1>
       <div class="col-lg-6 mx-auto">
         <p class="lead mb-4">
-          Vertex Racing was founded in 2020 with the purpose of taking part in
-          the 2021 F1 in schools competition, the team is made up of five
-          members.
+          Don’t hesitate to reach out with the contact information below, or
+          send a message using the form, whether you would like to contact about
+          prospective sponsorship, general questions we would love to hear from
+          you!
         </p>
       </div>
     </div>
 
     <div class="container px-4 py-5 marketing" id="featured-3">
-      <h2 class="pb-2 border-bottom">Meet the team</h2>
+      <h2 class="pb-2 border-bottom">From our socials</h2>
       <div class="row pt-4">
-        <div class="col-lg-4">
-          <img
-            src="~/assets/faces/june.png"
-            class="img rounded-circle face"
-            alt="..."
-          />
+        <iframe src="https://instagram.com/vertexracingf1"></iframe>
+      </div>
+    </div>
 
-          <h2 class="pt-3">June</h2>
-          <p>
-            June is from South Korean student with a passion for Medicine and
-            essay writing.
-            <!-- Therefore, he joined the team to transfer his research and
-            persuasive skills to the benefit of Vertex Racing. He leads, manages
-            and organises the team to all it to achieve its goals. He is also in
-            charge of the production of the team portfolios (Enterprise and
-            Research & Development) and pit display on Adobe Illustrator and
-            InDesign, as well as the maintenance of the company email and
-            website. When he has spare time, June also runs fluid simulations of
-            the car on Solidworks to help Justin maximise the car aerodynamic
-            performance.-->
-          </p>
-          <p><a class="btn btn-secondary" href="#">View details »</a></p>
-        </div>
-        <div class="col-lg-4">
-          <img
-            src="~/assets/faces/dylan.png"
-            class="img rounded-circle face"
-            alt="..."
-          />
-
-          <h2 class="pt-3">Dylan</h2>
-          <p>Server guy...</p>
-          <p><a class="btn btn-secondary" href="#">View details »</a></p>
-        </div>
-        <div class="col-lg-4">
-          <img
-            src="~/assets/faces/vincent.png"
-            class="img rounded-circle face"
-            alt="..."
-          />
-
-          <h2 class="pt-3">Vincent</h2>
-          <p>AI guy...</p>
-          <p><a class="btn btn-secondary" href="#">View details »</a></p>
-        </div>
-        <!-- /.col-lg-4 -->
-        <div class="col-lg-4">
-          <img
-            src="~/assets/faces/justin.png"
-            class="img rounded-circle face"
-            alt="..."
-          />
-
-          <h2 class="pt-3">Justin</h2>
-          <p>Mechanical guy...</p>
-          <p><a class="btn btn-secondary" href="#">View details »</a></p>
-        </div>
-        <!-- /.col-lg-4 -->
-        <div class="col-lg-4">
-          <svg
-            class="bd-placeholder-img img rounded-circle face"
-            xmlns="http://www.w3.org/2000/svg"
-            role="img"
-            aria-label="Placeholder"
-            preserveAspectRatio="xMidYMid slice"
-            focusable="false"
-          >
-            <title>Placeholder</title>
-            <rect width="100%" height="100%" fill="#868e96"></rect>
-          </svg>
-
-          <h2 class="pt-3">Andrew</h2>
-          <p>Rugby guy...</p>
-          <p><a class="btn btn-secondary" href="#">View details »</a></p>
-        </div>
-        <!-- /.col-lg-4 -->
+    <div class="container px-4 py-5 marketing" id="featured-3">
+      <h2 class="pb-2 border-bottom">Enquiries</h2>
+      <div class="row pt-4">
+        <iframe
+          width="640px"
+          height="480px"
+          src="https://forms.office.com/Pages/ResponsePage.aspx?id=UBzjllrT0UicvFq7UcsWiEIssA9j1A5GsW-Uzoc94INUMzEwV0FMWlJNNFlER1AyWTVUWlZDRFlGWC4u&embed=true"
+          frameborder="0"
+          marginwidth="0"
+          marginheight="0"
+          style="border: none; max-width: 100%; max-height: 100vh"
+          allowfullscreen
+          webkitallowfullscreen
+          mozallowfullscreen
+          msallowfullscreen
+        >
+        </iframe>
       </div>
     </div>
   </div>
