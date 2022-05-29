fd3f4d2227e4cfaf4d20e0166f44f5ab3586db43
Dylan K <50585244+dylankainth@users.noreply.github.com>
2022-05-29 23:21:28 +0100

    added quote

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -7,6 +7,19 @@
     />
 
     <div class="container">
+      <div class="px-4 py-5 my-5 text-center" data-aos="zoom-in-up">
+        <h1 class="display-6 fw-bold">
+          "The car looks like a highly decorated bullet"
+        </h1>
+        <div class="col-lg-6 mx-auto">
+          <p class="lead mb-4">
+            - George C, Circa may 2022.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
       <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
         <div class="col" v-for="image in images" :key="image">
           <div
