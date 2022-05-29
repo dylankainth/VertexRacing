4e86c1f05d92fd295672c674ee3afd081d70d3f5
Dylan K <50585244+dylankainth@users.noreply.github.com>
2022-05-29 23:44:54 +0100

    may -> May

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -13,7 +13,7 @@
         </h1>
         <div class="col-lg-6 mx-auto">
           <p class="lead mb-4">
-            - George C, Circa may 2022.
+            - George C, Circa May 2022.
           </p>
         </div>
       </div>
